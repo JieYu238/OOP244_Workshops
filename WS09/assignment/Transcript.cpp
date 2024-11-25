25906b2d90eb00ae76cf8a536b12caa5b4adc1ff
Jie Yu <jyu238@myseneca.ca>
2024-11-25 15:08:49 -0500

    modify date submitted

--- WS09/assignment/Transcript.cpp
+++ WS09/assignment/Transcript.cpp
@@ -2,7 +2,7 @@
 Name:   Jie Yu
 Student ID: 152519237
 Email:  jyu238@myseneca.ca
-Date:   20241122
+Date:   20241129
 *********************************************************************************************************************************
 I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
 *********************************************************************************************************************************
